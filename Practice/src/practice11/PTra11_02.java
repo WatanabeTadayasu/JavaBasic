406da14964dc344648776c6fa7042b2c90b6bbb9
LAPTOP-HVPLLLQ5\tadayasu <tadayasu.watanabe.likeit@gmail.com>
2018-11-07 11:14:30 +0900

    11-2

--- Practice/src/practice11/PTra11_02.java
+++ Practice/src/practice11/PTra11_02.java
@@ -11,16 +11,20 @@ public class PTra11_02 {
 
 		// ★ ReadFileClassのクラスメソッドreadBookDataFile()を使い、本情報を取得します
 		//int random = car.run();
-		Book[] bookList = FileReaderClass.readBookDataFile();
-		
-		
+		Book[] b = FileReaderClass.readBookDataFile();
 
 		System.out.println("探したい本のタイトル（又はその一部）を入力してください");
 		// ★ ユーザが入力した文字列が、本情報のタイトル一部に含まれていた場合は、その本情報を出力してください
-		
+
 		java.util.Scanner scanner = new java.util.Scanner(System.in);
 		String line = scanner.nextLine();
-		
-		
+
+		for(int i = 0; i < b.length; i++) {
+
+			if(b[i].title.indexOf(line) != -1) {
+
+				System.out.println(b[i].dispBookInfo());
+			}
+		}
 	}
 }
